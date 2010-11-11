5ecfd73d94129989c0da2ec5ea400fed9dc7bbe3
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-11 22:21:05 +0100

    Corrected bashEntryAddForm

--- tar/files/lib/form/BashEntryAddForm.class.php
+++ tar/files/lib/form/BashEntryAddForm.class.php
@@ -6,23 +6,87 @@ require_once(WCF_DIR.'lib/form/MessageForm.class.php');
 require_once(BASH_DIR.'lib/data/bash/BashEntryEditor.class.php');
 require_once(BASH_DIR.'lib/data/server/Server.class.php');
 
+/**
+ * Implements a form that allows users to add bash entries
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.bash
+ */
 class BashEntryAddForm extends MessageForm {
+	
+	/**
+	 * @see AbstractPage::$templateName
+	 */
 	public $templateName = 'bashEntryAdd';
+	
+	/**
+	 * @see	CaptchaForm::$useCaptcha
+	 */
 	public $useCaptcha = true;
+	
+	/**
+	 * @see	MessageForm::$showSmilies
+	 */
 	public $showSmilies = true;
-	public $showSettings = false;
+	
+	/**
+	 * @see	MessageForm::$showSettings
+	 */
+	public $showSettings = true;
+	
+	/**
+	 * @see MessageForm::$showAttachments
+	 */
 	public $showAttachments = false;
+	
+	/**
+	 * @see	MessageForm::$showPoll
+	 */
 	public $showPoll = false;
+	
+	/**
+	 * @see	MessageForm::$showSignatureSetting
+	 */
 	public $showSignatureSetting = false;
+	
+	/**
+	 * @see	AbstractPage::$neededPermissions
+	 */
 	public $neededPermissions = 'user.bash.canAddBashEntry';
 	
+	/**
+	 * Contains a list of all default servers
+	 * @var	string
+	 */
 	public $serverList = array();
 	
+	/**
+	 * A little variable to pass validate method
+	 * @var	string
+	 */
 	public $subject = 'ThisIsAnEasteregg';
+	
+	/**
+	 * Contains the username of a guest
+	 * @var	string
+	 */
 	public $username = '';
+	
+	/**
+	 * Contains the ID of the server
+	 * @var	string
+	 */
 	public $serverID = 0;
+	
+	/**
+	 * Contains the server name
+	 * @var	string
+	 */
 	public $serverName = "";
 	
+	/**
+	 * @see	Page::readData()
+	 */
 	public function readData() {
 		parent::readData();
 		
@@ -40,6 +104,9 @@ class BashEntryAddForm extends MessageForm {
 		if (isset($_POST['serverName']))	$this->serverName	= StringUtil::trim($_POST['serverName']);
 	}
 	
+	/**
+	 * @see	Form::validate()
+	 */
 	public function validate() {
 		parent::validate();
 		
@@ -78,9 +145,11 @@ class BashEntryAddForm extends MessageForm {
 		}
 	}
 	
+	/**
+	 * @see	Form:.save()
+	 */
 	public function save() {
 		parent::save();
-		
 		
 		$entry = BashEntryEditor::create(BASHCore::getUser()->userID, $this->username, $this->serverID, $this->serverName, $this->text, TIME_NOW);
 		
@@ -90,8 +159,14 @@ class BashEntryAddForm extends MessageForm {
 		}
 		
 		HeaderUtil::redirect('index.php?page=BashEntry&entryID='.$entry->entryID.SID_ARG_2ND_NOT_ENCODED);
+		
+		// call event
+		$this->saved();
 	}
 	
+	/**
+	 * @see	Page::show()
+	 */
 	public function show() {
 		require_once(WCF_DIR.'lib/page/util/menu/PageMenu.class.php');
 		if (PageMenu::getActiveMenuItem() == '') PageMenu::setActiveMenuItem('bash.header.menu.addBash');
@@ -99,6 +174,9 @@ class BashEntryAddForm extends MessageForm {
 		parent::show();
 	}
 	
+	/**
+	 * @see	Page::assignVariables()
+	 */
 	public function assignVariables() {
 		parent::assignVariables();
 		
