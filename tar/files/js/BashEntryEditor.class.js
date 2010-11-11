a132891871170a1c3357732b26d9b686521686c8
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-11 21:48:03 +0100

    Added correct AJAX-Requests (With error handling)

--- tar/files/js/BashEntryEditor.class.js
+++ tar/files/js/BashEntryEditor.class.js
@@ -8,19 +8,38 @@
 var BashEntryEditor = {
 	
 		votePositive			:			function(entryID) {
-			$j.ajax({url: 'index.php?action=BashEntryVotePositive&ajax=1&entryID='+entryID+SID_ARG_2ND });
-			$('vote'+entryID+'Positive').fade();
-			$('vote'+entryID+'Negative').fade();
+			$j.ajax({
+				url: 'index.php?action=BashEntryVotePositive&ajax=1&entryID='+entryID+SID_ARG_2ND,
+				success: function() {
+					$('vote'+entryID+'Positive').fade();
+					$('vote'+entryID+'Negative').fade();
+				},
+				error: function() {
+					Effect.Shake('vote'+entryID+'Positive');
+				}});
 		},
 		
 		voteNegative			:			function(entryID) {
-			$j.ajax({url: 'index.php?action=BashEntryVoteNegative&ajax=1&entryID='+entryID+SID_ARG_2ND });
-			$('vote'+entryID+'Positive').fade();
-			$('vote'+entryID+'Negative').fade();
+			$j.ajax({
+				url: 'index.php?action=BashEntryVoteNegative&ajax=1&entryID='+entryID+SID_ARG_2ND,
+				success: function() {
+					$('vote'+entryID+'Positive').fade();
+					$('vote'+entryID+'Negative').fade();
+				},
+				error: function() {
+					Effect.Shake('vote'+entryID+'Negative');
+				}});
 		},
 		
 		remove					:			function(entryID) {
-			$j.ajax({url: 'index.php?action=BashEntryDelete&ajax=1&entryID='+entryID+SID_ARG_2ND });
-			$('entry' + entryID + 'Container').fade();
-		}
+			$j.ajax({
+				url: 'index.php?action=BashEntryDelete&ajax=1&entryID='+entryID+SID_ARG_2ND,
+				success: function() {
+					$('entry' + entryID + 'Container').fade();
+				},
+				error: function() {
+					Effect.Shake('entry' + entryID + 'Container');
+				}
+			});
+		},
 };
