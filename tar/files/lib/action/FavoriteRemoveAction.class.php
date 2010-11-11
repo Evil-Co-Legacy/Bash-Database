cfc6cb288e8ecc11791834f46cee931c9deb8912
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-11 21:43:25 +0100

    Corrected FavoriteRemoveAction

--- tar/files/lib/action/FavoriteRemoveAction.class.php
+++ tar/files/lib/action/FavoriteRemoveAction.class.php
@@ -5,9 +5,22 @@ require_once(WCF_DIR.'lib/action/AbstractAction.class.php');
 // bash imports
 require_once(BASH_DIR.'lib/data/bash/BashEntryEditor.class.php');
 
+/**
+ * Implements an action that removes a bash entry von favorites list
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.bash
+ */
 class FavoriteRemoveAction extends AbstractAction {
+	
+	/**
+	 * @see	BashEntryDeleteAction::$entryID
+	 */
 	public $entryID = 0;
 	
+	/**
+	 * @see	Action::readParameters()
+	 */
 	public function readParameters() {
 		parent::readParameters();
 		
@@ -16,6 +29,9 @@ class FavoriteRemoveAction extends AbstractAction {
 		if (isset($_REQUEST['entryID'])) $this->entryID = intval($_REQUEST['entryID']);
 	}
 	
+	/**
+	 * @see	Action::execute()
+	 */
 	public function execute() {
 		parent::execute();
 		
@@ -26,7 +42,7 @@ class FavoriteRemoveAction extends AbstractAction {
 			$entry->unfavorite();
 		}
 		
-		HeaderUtil::redirect(BASHCore::getSession()->lastRequestURI, false);
+		if (!isset($_REQUEST['ajax'])) HeaderUtil::redirect(BASHCore::getSession()->lastRequestURI, false);
 	}
 }
 ?>
