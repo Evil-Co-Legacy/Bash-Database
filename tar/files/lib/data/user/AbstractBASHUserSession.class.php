d56e3794fed6611b12d0d7fcda0513cb37c128de
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-11 21:22:16 +0100

    Added missing comments

--- tar/files/lib/data/user/AbstractBASHUserSession.class.php
+++ tar/files/lib/data/user/AbstractBASHUserSession.class.php
@@ -1,6 +1,12 @@
 <?php
 require_once(WCF_DIR.'lib/system/session/UserSession.class.php');
 
+/**
+ * Represents a bash session
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.bash
+ */
 class AbstractBASHUserSession extends UserSession {
 	
 }
