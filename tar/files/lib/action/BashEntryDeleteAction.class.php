512316a1d55581e45c5c1f4f891511a94ca39a94
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-11 21:27:19 +0100

    Corrected BashEntryDeleteAction

--- tar/files/lib/action/BashEntryDeleteAction.class.php
+++ tar/files/lib/action/BashEntryDeleteAction.class.php
@@ -5,9 +5,23 @@ require_once(WCF_DIR.'lib/action/AbstractAction.class.php');
 // bash imports
 require_once(BASH_DIR.'lib/data/bash/BashEntryEditor.class.php');
 
+/**
+ * Implements an action that removes bash entry rows
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.bash
+ */
 class BashEntryDeleteAction extends AbstractAction {
+	
+	/**
+	 * Contains the entryID of the row that should removed
+	 * @var	integer
+	 */
 	public $entryID = 0;
 	
+	/**
+	 * @see	Action::readParameters()
+	 */
 	public function readParameters() {
 		parent::readParameters();
 		
@@ -16,6 +30,9 @@ class BashEntryDeleteAction extends AbstractAction {
 		if (isset($_REQUEST['entryID'])) $this->entryID = intval($_REQUEST['entryID']);
 	}
 	
+	/**
+	 * @see	Action::execute()
+	 */
 	public function execute() {
 		parent::execute();
 		
@@ -26,7 +43,7 @@ class BashEntryDeleteAction extends AbstractAction {
 		
 		BashEntryEditor::remove($entry->entryID);
 		
-		HeaderUtil::redirect(BASHCore::getSession()->lastRequestURI, false);
+		if (!isset($_REQUEST['ajax'])) HeaderUtil::redirect(BASHCore::getSession()->lastRequestURI, false);
 	}
 }
 ?>
