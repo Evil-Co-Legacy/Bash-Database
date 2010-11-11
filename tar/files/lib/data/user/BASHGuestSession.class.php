0675e7e6500e6d79a1b51266dd04e5365421f500
Johannes Donath <webmaster@mail.punksoft.net>
2010-11-11 21:21:40 +0100

    Corrected BASHGuestSession

--- tar/files/lib/data/user/BASHGuestSession.class.php
+++ tar/files/lib/data/user/BASHGuestSession.class.php
@@ -1,7 +1,19 @@
 <?php
 require_once(BASH_DIR.'lib/data/user/AbstractBASHUserSession.class.php');
 
+/**
+ * Represents a bash guest session
+ * @author		Johannes Donath
+ * @copyright	2010 DEVel Fusion
+ * @package		de.evil-co.bash
+ */
 class BASHGuestSession extends AbstractBASHUserSession {
 	
+	/**
+	 * @see	BASHUserSession::isModerator()
+	 */
+	public function isModerator() {
+		return false;
+	}
 }
 ?>
